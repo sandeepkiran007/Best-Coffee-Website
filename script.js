e25dec492185cf1a13d27a0630390ca5ea71ad7c
sandeep kiran <sandeepkiran539@gamil.com>
2024-11-02 18:28:37 +0530

    complete script for the website

---
+++ script.js
@@ -0,0 +1,47 @@
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+const navLinks = document.querySelectorAll(".nav-menu .nav-link")
+
+menuOpenButton.addEventListener("click", ()=> {
+    document.body.classList.toggle("show-mobile-menu");
+});
+
+navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        menuOpenButton.click()
+    })
+})
+
+menuCloseButton.addEventListener('click', () => menuOpenButton.click());
+
+const swiper = new Swiper('.slider-wrapper', {
+    
+    loop: true,
+    spaceBetween: 25,
+    grabCursor: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets:true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        }
+    }
+  });
